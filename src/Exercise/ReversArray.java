dff870c82095f2a66a62886a3a8b97ba5f939163
phuonglinh <phuonglinh@localhost>
2026-08-26 20:28:58 +0700

    Rename ReversArrya to ReversArray

--- src/Exercise/ReversArray.java
+++ src/Exercise/ReversArray.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author phuonglinh
  */
-public class ReversArrya {
+public class ReversArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
